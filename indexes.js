a3f45054425e2f58f0539e3dad668c2b9ec7d2f8
likhithag0328 <likhithagundiga@gmail.com>
2024-11-21 22:10:08 +0530

    Create indexes.js

---
+++ indexes.js
@@ -0,0 +1,10 @@
+//blue   yellow
+
+//white  green
+
+const pathIndex = '0520-0525-0530-0421-0307-0193-0079-0084-0089-0203-0316-0431-0550-0555-0560-0565-0680-0794-0789-0784-0779-0888-1002-1116-1230-1225-1220-1106-0992-0878-0759-0754-0749-0744-0630-0515';//starting from first box of blue
+
+const blueFirstIndex = 520;
+const yellowFirstIndex = 203;
+const whiteFirstIndex = 789;
+const greenFirstIndex = 1106;
