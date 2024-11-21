3553612682addbd9ee3e54b4c26fedbda0330847
sushanth <153906968+sushanthj674@users.noreply.github.com>
2024-11-21 18:27:31 +0530

    Create allFunctions.js

---
+++ allFunctions.js
@@ -0,0 +1,11 @@
+const indexes = '0001-0003-0023-0123-1234';
+// 0001-0003-0023-0123-1234;
+// 0123456789012345678901234567890
+function sliceIndex(index){
+  let value = '';
+  const start = index * 5 
+  for(let i = start; i < start + 4 ; i++) {
+    value += indexes[i];
+  }
+  return +value;
+}
