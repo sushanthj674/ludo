886969218f40cb3e289cfbdec0882cffdc0cb598
sushanth <153906968+sushanthj674@users.noreply.github.com>
2024-11-25 13:00:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,31 +11,25 @@ const whitePath = '1106-0992-0878-0759-0754-0749-0744-0630-0515-0520-0525-0530-0
 
 const blueCircle = '🔵';
 const blueHeart = '💙';
-const blueSquare = '  ';
-const blueAnimal = '  ';
 
 const yellowCircle = '🟡';
 const yellowHeart = '💛';
-const yellowSquare = '  ';
-const yellowAnimal = '  ';
 
 const greenCircle = '🟢';
 const greenHeart = '💚';
-const greenSquare = '  ';
-const greenAnimal = '  ';
 
 const whiteCircle = '⚪️';
 const whiteHeart = '🤍';
-const whiteSquare = '  ';
-const whiteAnimal = '  ';
 
 const blueHeartInitialpos = 185;
 const yellowHeartInitialpos = 219;
+
 const greenHeartInitialpos = 1018;
 const whiteHeartInitialpos = 984;
 
 const blueCircleInitialpos = 179;
 const yellowCircleInitialpos = 213;
+
 const greenCircleInitialpos = 1012;
 const whiteCircleInitialpos = 978;
 
@@ -56,49 +50,12 @@ let whiteScore = 0;
 
 let recursionControlVar = 0;
 
-let blueC = "  ";
-let blueH = "  ";
-let blueS = "  ";
-let blueA = "  ";
+let box = '\n┏━━━━━━━━━━━━━━━━━━━┳━━━━┯━━━━┯━━━━┳━━━━━━━━━━━━━━━━━━━┓\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┆    ┆    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┟━━━━┪┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + blueCircle + '    ' + blueHeart + '    ┃┃    ┃    ┃    ┃┃    ' + yellowCircle + '    ' + yellowHeart + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃                ┃┃    ┃    ┃    ┃┃                ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┃    ┃    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┣━━━━┯━━━━┯━━━━┯━━━━╋━━━━┻━━━━┻━━━━╋━━━━┯━━━━┯━━━━┯━━━━┫\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃ \n┃┄┄┄┄┟━━━━╈━━━━╈━━━━┫              ┣━━━━╈━━━━╈━━━━┪┄┄┄┄┃\n┃    ┃    ┃    ┃    ┃              ┃    ┃    ┃    ┃    ┃\n┃┄┄┄┄┡━━━━╇━━━━╇━━━━┫              ┣━━━━╇━━━━╇━━━━╇┄┄┄┄┃\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃\n┣━━━━┷━━━━┷━━━━┷━━━━╋━━━━┳━━━━┳━━━━╋━━━━┷━━━━┷━━━━┷━━━━┫\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┃    ┃    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + whiteCircle + '    ' + whiteHeart + '    ┃┃    ┃    ┃    ┃┃    ' + greenCircle + '    ' + greenHeart + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃                ┃┃    ┃    ┃    ┃┃                ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┡━━━━╇┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┆    ┆    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┗━━━━━━━━━━━━━━━━━━━┻━━━━┷━━━━┷━━━━┻━━━━━━━━━━━━━━━━━━━┛';
 
-let yellowC = "  ";
-let yellowH = "  ";
-let yellowS = "  ";
-let yellowA = "  ";
-
-let whiteC = "  ";
-let whiteH = "  ";
-let whiteS = "  ";
-let whiteA = "  ";
-
-let greenC = "  ";
-let greenH = "  ";
-let greenS = "  ";
-let greenA = "  ";
-
-blueC = blueCircle;
-blueH = blueHeart;
-blueS = blueSquare;
-blueA = blueAnimal;
-
-yellowC = yellowCircle;
-yellowH = yellowHeart;
-yellowS = yellowSquare;
-yellowA = yellowAnimal;
-
-whiteC = whiteCircle;
-whiteH = whiteHeart;
-whiteS = whiteSquare;
-whiteA = whiteAnimal;
-
-greenC = greenCircle;
-greenH = greenHeart;
-greenS = greenSquare;
-greenA = greenAnimal;
-let box = '\n┏━━━━━━━━━━━━━━━━━━━┳━━━━┯━━━━┯━━━━┳━━━━━━━━━━━━━━━━━━━┓\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┆    ┆    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┟━━━━┪┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + blueC + '    ' + blueH + '    ┃┃    ┃    ┃    ┃┃    ' + yellowC + '    ' + yellowH + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + blueS + '    ' + blueA + '    ┃┃    ┃    ┃    ┃┃    ' + yellowS + '    ' + yellowA + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┃    ┃    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┣━━━━┯━━━━┯━━━━┯━━━━╋━━━━┻━━━━┻━━━━╋━━━━┯━━━━┯━━━━┯━━━━┫\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃ \n┃┄┄┄┄┟━━━━╈━━━━╈━━━━┫              ┣━━━━╈━━━━╈━━━━┪┄┄┄┄┃\n┃    ┃    ┃    ┃    ┃              ┃    ┃    ┃    ┃    ┃\n┃┄┄┄┄┡━━━━╇━━━━╇━━━━┫              ┣━━━━╇━━━━╇━━━━╇┄┄┄┄┃\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃\n┣━━━━┷━━━━┷━━━━┷━━━━╋━━━━┳━━━━┳━━━━╋━━━━┷━━━━┷━━━━┷━━━━┫\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┃    ┃    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + whiteC + '    ' + whiteH + '    ┃┃    ┃    ┃    ┃┃    ' + greenC + '    ' + greenH + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    ' + whiteS + '    ' + whiteA + '    ┃┃    ┃    ┃    ┃┃    ' + greenS + '    ' + greenA + '    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┡━━━━╇┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┆    ┆    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┗━━━━━━━━━━━━━━━━━━━┻━━━━┷━━━━┷━━━━┻━━━━━━━━━━━━━━━━━━━┛';
 function rollADice() {
   return Math.ceil(Math.random() * 6);
 }
+
 function printBox() {
   console.clear();
   console.log(ludoBigLetters);
@@ -106,6 +63,10 @@ function printBox() {
     console.log("\nyou got !!! " + diceVal);
   }
   console.log(box);
+}
+
+function isTokenHeart(token) {
+  return token === '💙' || token === '💛' || token === '💚' || token === '🤍';
 }
 
 function getCellAdderss(addressBook, cellNumber) {
@@ -122,39 +83,33 @@ function CheckAndKill(colorAddressBook, nextPosition) {
   const movingAddress = getCellAdderss(colorAddressBook, nextPosition);
   if (colorAddressBook !== greenPath && movingAddress === getCellAdderss(greenPath, greenCirclepos)) {
     greenCirclepos = -1;
-    moveto(movingAddress, greenCircleInitialpos, greenCircle);
+    moveToken(movingAddress, greenCircleInitialpos, greenCircle);
   } else if (colorAddressBook !== greenPath && movingAddress === getCellAdderss(greenPath, greenHeartpos)) {
     greenHeartpos = -1;
-    moveto(box.lastIndexOf(greenHeart) - 2, greenHeartInitialpos, greenHeart);
+    moveToken(box.lastIndexOf(greenHeart) - 2, greenHeartInitialpos, greenHeart);
   } else if (colorAddressBook !== yellowPath && movingAddress === getCellAdderss(yellowPath, yellowHeartpos)) {
     yellowHeartpos = -1;
-    moveto(box.lastIndexOf(yellowHeart) - 2, yellowHeartInitialpos, yellowHeart);
+    moveToken(box.lastIndexOf(yellowHeart) - 2, yellowHeartInitialpos, yellowHeart);
   } else if (colorAddressBook !== yellowPath && movingAddress === getCellAdderss(yellowPath, yellowCirclepos)) {
     yellowCirclepos = -1;
-    moveto(movingAddress, yellowCircleInitialpos, yellowCircle);
+    moveToken(movingAddress, yellowCircleInitialpos, yellowCircle);
   } else if (colorAddressBook !== whitePath && movingAddress === getCellAdderss(whitePath, whiteHeartpos)) {
     whiteHeartpos = -1;
-    moveto(box.lastIndexOf(whiteHeart) - 2, whiteHeartInitialpos, whiteHeart);
+    moveToken(box.lastIndexOf(whiteHeart) - 2, whiteHeartInitialpos, whiteHeart);
   } else if (colorAddressBook !== whitePath && movingAddress === getCellAdderss(whitePath, whiteCirclepos)) {
     whiteCirclepos = -1;
-    moveto(movingAddress, whiteCircleInitialpos, whiteCircle);
+    moveToken(movingAddress, whiteCircleInitialpos, whiteCircle);
   } else if (colorAddressBook !== bluePath && movingAddress === getCellAdderss(bluePath, blueHeartpos)) {
     blueHeartpos = -1;
-    moveto(box.lastIndexOf(blueHeart) - 2, blueHeartInitialpos, blueHeart);
+    moveToken(box.lastIndexOf(blueHeart) - 2, blueHeartInitialpos, blueHeart);
   } else if (colorAddressBook !== bluePath && movingAddress === getCellAdderss(bluePath, blueCirclepos)) {
     blueCirclepos = -1;
-    moveto(movingAddress, blueCircleInitialpos, blueCircle);
+    moveToken(movingAddress, blueCircleInitialpos, blueCircle);
   }
 }
 
-function validateScores() {
-  greenScore = (greenCirclepos + greenHeartpos) / 2;
-  yellowScore = (yellowCirclepos + yellowHeartpos) / 2;
-  blueScore = (blueCirclepos + blueHeartpos) / 2;
-  whiteScore = (whiteCirclepos + whiteHeartpos) / 2;
-}
 
-function moveto(currentPosition, nextPosition, replacement) {
+function moveToken(currentPosition, nextPosition, replacement) {
   let newBox = "";
   let space = 0;
   if (replacement === '💙' || replacement === '💛' || replacement === '💚' || replacement === '🤍') {
@@ -173,53 +128,35 @@ function moveto(currentPosition, nextPosition, replacement) {
 
   box = newBox;
 }
+
 let diceVal = 0;
-
-function executeBlueHeart(diceVal) {
-  blueHeartpos += blueHeartpos === -1 ? 1 : diceVal;
-  const currentPos = getCellAdderss(bluePath, blueHeartpos);
-
-  CheckAndKill(bluePath, blueHeartpos);
-  moveto(box.lastIndexOf('💙') - 2, currentPos, blueHeart);
-}
-
-function executeBlueCircle() {
-  if (blueCirclepos + diceVal <= 38) {
-
-    blueCirclepos += blueCirclepos === -1 ? 1 : diceVal;;
-    const currentPos = getCellAdderss(bluePath, blueCirclepos);
-
-    CheckAndKill(bluePath, blueCirclepos);
-    moveto(box.lastIndexOf(blueCircle), currentPos, blueCircle);
-  }
-}
 // ----------------------------------------------------------------------
 function executeBlue() {
   if (blueCirclepos === -1 && diceVal !== 6) {
     if (blueHeartpos === -1) {
       return '';
     }
-    return executeBlueHeart();
+    return executeToken(blueHeartpos, bluePath, blueHeart);
   }
   if (blueHeartpos === -1 && diceVal !== 6) {
     if (blueCirclepos === -1) {
       return '';
     }
-    return executeBlueCircle();
+    return executeToken(blueCirclepos, bluePath, blueCircle);
   }
 
   if (blueCirclepos + diceVal <= 38 && blueHeartpos + diceVal <= 38) {
     if (confirm(" press 'Y' to move heart ")) {
-      return executeBlueHeart(diceVal);
+      return executeToken(blueHeartpos, bluePath, blueHeart);
     } else {
-      return executeBlueCircle(diceVal);
+      return executeToken(blueCirclepos, bluePath, blueCircle);
     }
 
   } else if (blueCirclepos + diceVal <= 38 || blueHeartpos + diceVal <= 38) {
     if (blueCirclepos + diceVal <= 38) {
-      return executeBlueCircle(diceVal);
+      return executeToken(blueCirclepos, bluePath, blueCircle);
     } else {
-      return executeBlueHeart(diceVal);
+      return executeToken(blueHeartpos, bluePath, blueHeart);
     }
   }
 
@@ -230,151 +167,142 @@ function executeYellow() {
     if (yellowHeartpos === -1) {
       return '';
     }
-    return executeYellowHeart();
+    return executeToken(yellowHeartpos, yellowPath, yellowHeart);
   }
   if (yellowHeartpos === -1 && diceVal !== 6) {
     if (yellowCirclepos === -1) {
       return '';
     }
-    return executeYellowCircle();
+    return executeToken(yellowCirclepos, yellowPath, yellowCircle);
   }
 
   if (yellowCirclepos + diceVal <= 38 && yellowHeartpos + diceVal <= 38) {
     if (confirm(" press 'Y' to move heart ")) {
-      return executeYellowHeart(diceVal);
+      return executeToken(yellowHeartpos, yellowPath, yellowHeart);
     } else {
-      return executeYellowCircle(diceVal);
+      return executeToken(yellowCirclepos, yellowPath, yellowCircle);
     }
 
   } else if (yellowCirclepos + diceVal <= 38 || yellowHeartpos + diceVal <= 38) {
     if (yellowCirclepos + diceVal <= 38) {
-      return executeYellowCircle(diceVal);
+      return executeToken(yellowCirclepos, yellowPath, yellowCircle);
     } else {
-      return executeYellowHeart(diceVal);
+      return executeToken(yellowHeartpos, yellowPath, yellowHeart);
     }
   }
 }
 
-function executeYellowCircle() {
-  if (yellowCirclepos + diceVal <= 38) {
+function executeToken(tokenPosition, path, token) {
+  if (tokenPosition + diceVal <= 38) {
 
-    yellowCirclepos += yellowCirclepos === -1 ? 1 : diceVal;;
-    const currentPos = getCellAdderss(yellowPath, yellowCirclepos);
+    tokenPosition += tokenPosition === -1 ? 1 : diceVal;
+    updateTokenPos(token, tokenPosition);
+    const currentPos = getCellAdderss(path, tokenPosition);
 
-    CheckAndKill(yellowPath, yellowCirclepos);
-    moveto(box.lastIndexOf(yellowCircle), currentPos, yellowCircle);
+    CheckAndKill(path, tokenPosition);
+    let currentIndex = box.lastIndexOf(token);
+    currentIndex -= isTokenHeart(token) ? 2 : 0;
+    moveToken(currentIndex, currentPos, token);
   }
 }
-function executeYellowHeart() {
-  yellowHeartpos += yellowHeartpos === -1 ? 1 : diceVal;;
-  const currentPos = getCellAdderss(yellowPath, yellowHeartpos);
 
-  CheckAndKill(yellowPath, yellowHeartpos);
-  moveto(box.lastIndexOf(yellowHeart) - 2, currentPos, yellowHeart);
-}
+function updateTokenPos(token, position) {
+  console.log(ludoBigLetters);
 
-function executeWhiteHeart() {
-  whiteHeartpos += whiteHeartpos === -1 ? 1 : diceVal;;
-  const currentPos = getCellAdderss(whitePath, whiteHeartpos);
-
-  CheckAndKill(whitePath, whiteHeartpos);
-  moveto(box.lastIndexOf('🤍') - 2, currentPos, whiteHeart);
-}
-
-function executeWhiteCircle() {
-  if (whiteCirclepos + diceVal <= 38) {
-
-    whiteCirclepos += whiteCirclepos === -1 ? 1 : diceVal;;
-    const currentPos = getCellAdderss(whitePath, whiteCirclepos);
-
-    CheckAndKill(whitePath, whiteCirclepos);
-    moveto(box.lastIndexOf(whiteCircle), currentPos, whiteCircle);
+  switch (token) {
+    case blueCircle:
+      blueCirclepos = position;
+      break;
+    case blueHeart:
+      blueHeartpos = position;
+      break;
+    case yellowCircle:
+      yellowCirclepos = position;
+      break;
+    case yellowHeart:
+      yellowHeartpos = position;
+      break;
+    case greenCircle:
+      greenCirclepos = position;
+      break;
+    case greenHeart:
+      greenHeartpos = position;
+      break;
+    case whiteHeart:
+      whiteHeartpos = position;
+      break;
+    case whiteCircle:
+      whiteCirclepos = position;
+      break;
   }
 }
-// ----------------------------------------------------------------------
+
 function executeWhite() {
   if (whiteCirclepos === -1 && diceVal !== 6) {
     if (whiteHeartpos === -1) {
       return '';
     }
-    return executeWhiteHeart();
+    return executeToken(whiteHeartpos, whitePath, whiteHeart);
   }
   if (whiteHeartpos === -1 && diceVal !== 6) {
     if (whiteCirclepos === -1) {
       return '';
     }
-    return executeWhiteCircle();
+    return executeToken(whiteCirclepos, whitePath, whiteCircle);
   }
 
   if (whiteCirclepos + diceVal <= 38 && whiteHeartpos + diceVal <= 38) {
     if (confirm(" press 'Y' to move heart ")) {
-      return executeWhiteHeart();
+      // return executeWhiteHeart();
+      return executeToken(whiteHeartpos, whitePath, whiteHeart);
     } else {
-      return executeWhiteCircle();
+      // return executeWhiteCircle();
+      return executeToken(whiteCirclepos, whitePath, whiteCircle);
+
     }
 
   } else if (whiteCirclepos + diceVal <= 38 || whiteHeartpos + diceVal <= 38) {
     if (whiteCirclepos + diceVal <= 38) {
-      return executeWhiteCircle(diceVal);
+      return executeToken(whiteCirclepos, whitePath, whiteCircle);
     } else {
-      return executeWhiteHeart(diceVal);
+      return executeToken(whiteHeartpos, whitePath, whiteHeart);
     }
   }
 
 }
-function executeGreenHeart() {
-  greenHeartpos += greenHeartpos === -1 ? 1 : diceVal;
-  const currentPos = getCellAdderss(greenPath, greenHeartpos);
 
-  CheckAndKill(greenPath, greenHeartpos);
-  moveto(box.lastIndexOf('💚') - 2, currentPos, greenHeart);
-}
-
-function executeGreenCircle() {
-  if (greenCirclepos + diceVal <= 38) {
-
-    greenCirclepos += greenCirclepos === -1 ? 1 : diceVal;
-    const currentPos = getCellAdderss(greenPath, greenCirclepos);
-
-    CheckAndKill(greenPath, greenCirclepos);
-    moveto(box.lastIndexOf(greenCircle), currentPos, greenCircle);
-  }
-}
 // ----------------------------------------------------------------------
 function executeGreen() {
   if (greenCirclepos === -1 && diceVal !== 6) {
     if (greenHeartpos === -1) {
       return '';
     }
-    return executeGreenHeart();
+    return executeToken(greenHeartpos, greenPath, greenHeart);
   }
   if (greenHeartpos === -1 && diceVal !== 6) {
     if (greenCirclepos === -1) {
       return '';
     }
-    return executeGreenCircle();
+    return executeToken(greenCirclepos, greenPath, greenCircle);
   }
   if (greenCirclepos + diceVal <= 38 && greenHeartpos + diceVal <= 38) {
     if (confirm(" press 'Y' to move heart ")) {
-      return executeGreenHeart();
+      return executeToken(greenHeartpos, greenPath, greenHeart);
     } else {
-      return executeGreenCircle();
+      return executeToken(greenCirclepos, greenPath, greenCircle);
     }
 
   } else if (greenCirclepos + diceVal <= 38 || greenHeartpos + diceVal <= 38) {
     if (greenCirclepos + diceVal <= 38) {
-      return executeGreenCircle();
+      return executeToken(greenCirclepos, greenPath, greenCircle);
     } else {
-      return executeGreenHeart();
+      return executeToken(greenHeartpos, greenPath, greenHeart);
     }
   }
 
 }
-
 function gameCore() {
-
   printBox();
-  validateScores();
   if (greenHeartpos === 38 && greenCirclepos === 38) {
     return 'green won🏆 the match🎉🥳';
   } else if (yellowHeartpos === 38 && yellowCirclepos === 38) {
