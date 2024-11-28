1be0ca9bfe5c650ebc6ec4e3cd511230897eff2e
likhithag0328 <likhithagundiga@gmail.com>
2024-11-28 15:42:48 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,72 +1,24 @@
-
-let blueCircle = '🔵';
-let blueHeart = '💙';
-let blueSquare = '🟦';
-let blueAnimal = '🪼';
-
-let yellowCircle = '🟡';
-let yellowHeart = '💛';
-let yellowSquare = '🟨';
-let yellowAnimal = '🐥';
-
-let greenCircle = '🟢';
-let greenHeart = '💚';
-let greenSquare = '🟩';
-let greenAnimal = '🐢';
-
-let whiteCircle = '⚪️';
-let whiteHeart = '🤍';
-let whiteSquare = '⬜️';
-let whiteAnimal = '🐼';
-
-let blueC ="  ";
-let blueH ="  ";
-let blueS ="  ";
-let blueA ="  ";
-
-let yellowC ="  ";
-let yellowH ="  ";
-let yellowS ="  ";
-let yellowA ="  ";
-
-let whiteC ="  ";
-let whiteH ="  ";
-let whiteS ="  ";
-let whiteA ="  ";
-
-let greenC ="  ";
-let greenH ="  ";
-let greenS ="  ";
-let greenA ="  ";
-
-
-function printBox(){
-  const box = 
-'\n┏━━━━━━━━━━━━━━━━━━━┳━━━━┯━━━━┯━━━━┳━━━━━━━━━━━━━━━━━━━┓\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┆    ┆    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┟━━━━┪┄┄┄┄┃┃                ┃ ┃\n┃ ┃    '+ blueC +'    '+ blueH +'    ┃┃    ┃    ┃    ┃┃    '+ yellowC +'    '+ yellowH +'    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    '+ blueS +'    '+ blueA +'    ┃┃    ┃    ┃    ┃┃    '+ yellowS +'    '+ yellowA +'    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┃    ┃    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┣━━━━┯━━━━┯━━━━┯━━━━╋━━━━┻━━━━┻━━━━╋━━━━┯━━━━┯━━━━┯━━━━┫\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃ \n┃┄┄┄┄┟━━━━╈━━━━╈━━━━┫              ┣━━━━╈━━━━╈━━━━┪┄┄┄┄┃\n┃    ┃    ┃    ┃    ┃              ┃    ┃    ┃    ┃    ┃\n┃┄┄┄┄┡━━━━╇━━━━╇━━━━┫              ┣━━━━╇━━━━╇━━━━╇┄┄┄┄┃\n┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃\n┣━━━━┷━━━━┷━━━━┷━━━━╋━━━━┳━━━━┳━━━━╋━━━━┷━━━━┷━━━━┷━━━━┫\n┃ ┏━━━━━━━━━━━━━━━━┓┃    ┃    ┃    ┃┏━━━━━━━━━━━━━━━━┓ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    '+ whiteC +'    '+ whiteH +'    ┃┃    ┃    ┃    ┃┃    '+ greenC +'    '+ greenH +'    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃\n┃ ┃    '+ whiteS +'    '+ whiteA +'    ┃┃    ┃    ┃    ┃┃    '+ greenS +'    '+ greenA +'    ┃ ┃\n┃ ┃                ┃┃┄┄┄┄┡━━━━╇┄┄┄┄┃┃                ┃ ┃\n┃ ┗━━━━━━━━━━━━━━━━┛┃    ┆    ┆    ┃┗━━━━━━━━━━━━━━━━┛ ┃\n┗━━━━━━━━━━━━━━━━━━━┻━━━━┷━━━━┷━━━━┻━━━━━━━━━━━━━━━━━━━┛';
-console.log(box);
-}
-
-printBox();
-
-blueC = blueCircle;
-blueH = blueHeart;
-blueS = blueSquare;
-blueA = blueAnimal;
-
-yellowC = yellowCircle;
-yellowH = yellowHeart;
-yellowS = yellowSquare;
-yellowA = yellowAnimal;
-
-whiteC = whiteCircle;
-whiteH = whiteHeart;
-whiteS = whiteSquare;
-whiteA = whiteAnimal;
-
-greenC = greenCircle;
-greenH = greenHeart;
-greenS = greenSquare;
-greenA = greenAnimal;
-
-
-printBox();
+let box =`
+┏━━━━━━━━━━━━━━━━━━━┳━━━━┯━━━━┯━━━━┳━━━━━━━━━━━━━━━━━━━┓
+┃ ┏━━━━━━━━━━━━━━━━┓┃    ┆    ┆    ┃┏━━━━━━━━━━━━━━━━┓ ┃
+┃ ┃                ┃┃┄┄┄┄┟━━━━┪┄┄┄┄┃┃                ┃ ┃
+┃ ┃    ` + blueC + `    ` + blueH + `    ┃┃    ┃    ┃    ┃┃    ` + yellowC + `    ` + yellowH + `    ┃ ┃
+┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃
+┃ ┃    ` + blueS + `    ` + blueA + `    ┃┃    ┃    ┃    ┃┃    ` + yellowS + `    ` + yellowA + `    ┃ ┃
+┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃
+┃ ┗━━━━━━━━━━━━━━━━┛┃    ┃    ┃    ┃┗━━━━━━━━━━━━━━━━┛ ┃
+┣━━━━┯━━━━┯━━━━┯━━━━╋━━━━┻━━━━┻━━━━╋━━━━┯━━━━┯━━━━┯━━━━┫
+┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃ 
+┃┄┄┄┄┟━━━━╈━━━━╈━━━━┫              ┣━━━━╈━━━━╈━━━━┪┄┄┄┄┃
+┃    ┃    ┃    ┃    ┃              ┃    ┃    ┃    ┃    ┃
+┃┄┄┄┄┡━━━━╇━━━━╇━━━━┫              ┣━━━━╇━━━━╇━━━━╇┄┄┄┄┃
+┃    ┆    ┆    ┆    ┃              ┃    ┆    ┆    ┆    ┃
+┣━━━━┷━━━━┷━━━━┷━━━━╋━━━━┳━━━━┳━━━━╋━━━━┷━━━━┷━━━━┷━━━━┫
+┃ ┏━━━━━━━━━━━━━━━━┓┃    ┃    ┃    ┃┏━━━━━━━━━━━━━━━━┓ ┃
+┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃
+┃ ┃    ` + whiteC + `    ` + whiteH + `    ┃┃    ┃    ┃    ┃┃    ` + greenC + `    ` + greenH + `    ┃ ┃
+┃ ┃                ┃┃┄┄┄┄┣━━━━┫┄┄┄┄┃┃                ┃ ┃
+┃ ┃    ` + whiteS + `    ` + whiteA + `    ┃┃    ┃    ┃    ┃┃    ` + greenS + `    ` + greenA + `    ┃ ┃
+┃ ┃                ┃┃┄┄┄┄┡━━━━╇┄┄┄┄┃┃                ┃ ┃
+┃ ┗━━━━━━━━━━━━━━━━┛┃    ┆    ┆    ┃┗━━━━━━━━━━━━━━━━┛ ┃
+┗━━━━━━━━━━━━━━━━━━━┻━━━━┷━━━━┷━━━━┻━━━━━━━━━━━━━━━━━━━┛`;
